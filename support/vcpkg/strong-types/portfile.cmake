063c55b5a9f7852b2b0e44fbe5b26f17b776c4ca
de-passage <contact@sylvainleclercq.com>
2024-10-08 15:49:48 +0200

    Update port

--- support/vcpkg/strong-types/portfile.cmake
+++ support/vcpkg/strong-types/portfile.cmake
@@ -2,7 +2,7 @@ set(VCPKG_BUILD_TYPE release)
 vcpkg_from_git(
   OUT_SOURCE_PATH SOURCE_PATH
   URL https://github.com/de-passage/strong-types.cpp
-  REF 2a49789d6f7ee884b4aca0c9aa9f449dd088bb07
+  REF 850f343c55e4a48b389b2f1e5f6024607e87cbae
   HEAD_REF master
 )
 vcpkg_cmake_configure(
